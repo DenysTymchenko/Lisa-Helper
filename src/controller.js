f3fa3accbd2e3af05076ec8131b21a9b1412ea2b
Denys <tymchenko.denys@gmail.com>
2023-05-19 01:27:00 +0300

    renderLetters created

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,7 @@
-import { showNumbersReplacer, hideNumbersReplacer } from "./view.js"; 
+import { renderLetters, showNumbersReplacer, hideNumbersReplacer } from "./view.js"; 
+
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+letters.forEach((letter) => renderLetters(letter));
 
 const queryInput = document.getElementById('query');
 const numbersToggler = document.getElementById('numbers_toggler');
--- src/view.js
+++ src/view.js
@@ -1,3 +1,13 @@
+export function renderLetters(letter) {
+  const lettersDiv = document.querySelector('.work-panel__letters-wrapper');
+  
+  const letterP = document.createElement('p');
+  letterP.classList.add('work-panel__letter');
+  letterP.innerText = letter;
+
+  lettersDiv.append(letterP);
+}
+
 export function showNumbersReplacer() {
   const numbersReplacer = document.querySelector('.work-panel__numbers-replacer');
   numbersReplacer.classList.remove('d-none');
