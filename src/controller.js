e0e9fed94ed20f4debf0eeb3870470a1b71c8193
Denys <tymchenko.denys@gmail.com>
2023-05-19 02:33:57 +0300

    minor changes and bug fixes with desired number

--- src/controller.js
+++ src/controller.js
@@ -3,6 +3,7 @@ import { getNewQuery, getDefaultNumber } from './model.js';
 
 const queryInput = document.getElementById('query');
 const numbersToggler = document.getElementById('numbers_toggler');
+numbersToggler.checked = false;
 
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 letters.forEach((letter) => renderLetters(letter));
